0da67cb11942dd97cac9b92485be60b792cc9840
elchuev <orkhan.elchuev@stu.bahcesehir.edu.tr>
2018-03-02 12:26:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var deck =  ["fa-leaf", "fa-leaf","fa-bicycle", "fa-bicycle", "fa-bomb", "fa-bom
 var matchedCards = 0;
 var quantityOfMoves = 0;
 var numOfStars = 3;
-var timer = {seconds: 0, minutes: 0};
+var timer = {seconds: 0, minutes: 0, resetTime: -1};
 var open = [];
 var expert = 12;
 var amateur = 18;
@@ -26,46 +26,69 @@ function shuffle(array) {
     }
 
     return array;
-}
-
-// Mixing cards on the board by using (shuffle function)
-function updateCards() {
-    var index = 0;
-    deck = shuffle(deck);
-    $.each($(".card i"), function() { 
-      $(this).attr("class", "fa " + deck[index]);
-      index += 1; 
-    });
-    
-    timerRestart();
 };
 
-// Incrementing time , also checking for correct format of time
+// Starts the timer, ensures valid format of time in terms of sec. and min.
 function timerSet(){
+
+    // Make sure that amount of seconds wont be > than 59
     if(timer.seconds === 59){
       timer.seconds = 0;
       timer.minutes +=1;
     }else if(timer.seconds < 59){
       timer.seconds +=1;
     }
-}
+    var currentTime = String(timer.minutes) + ":" + String(timer.seconds);
+    $(.timer).text(currentTime);
+};
 
 // Restarts the time settings
-function timeRestart() {
-    clearInterval(timer.clearTime);
+function timerReset() {
+
+    // clears a timer set value
+    clearInterval(timer.resetTime);
     timer.seconds = 0;
     timer.minutes = 0;
-    $(".set-timer").text("0:00");
 
-    timer.clearTime = setInterval(timeSet, 2000);
+    // Updates the timer in HTML
+    $(".set-timer").text("0:00");
+    timer.resetTime = setInterval(timerSet, 2000);
 };
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+
+// Mixing cards on the board by using (shuffle function)
+function shuffleCards() {
+    var index = 0;
+    deck = shuffle(deck);
+
+    // Iterate through array of shuffled cards
+    $.each($(".card i"), function() {
+      $(this).attr("class", "fa " + deck[index]);
+      index += 1;
+    });
+    timerReset();
+};
+
+// Removes stars from list of stars
+function removeStar() {
+
+    // last() for removing last object from list of stars
+    $(".fa-star").last().attr("class", "fa fa-star-o");
+    numOfStars -= 1;
+
+    // Update the quantity of stars in HTML
+    $(".stars-quantity").text(String(numOfStars));
+};
+
+// Set the amount of stars to the default value (3)
+function resetStars() {
+    $(".fa-star-o").attr("class", "fa fa-star");
+    numOfStars = 3;
+
+    // Update the quantity of stars in HTML
+    $(".stars-quantity").text(String(numOfStars));
+};
+
+// Triggers winning condition
+function displayWinningMsg(){
+    popUpWin.css("display", "block");
+};
